ec8055c10a3f64adbd4acaa006d9244ad23054e9
Suhas SG <jargnar@gmail.com>
2015-02-22 18:10:49 +0530

    ENH: Add pelican source files

---
+++ pelicanconf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Suhas'
+SITENAME = u'The Coder Earth Blog'
+SITEURL = ''
+
+THEME = 'pelican-bootstrap3'
+BOOTSTRAP_THEME = 'cosmo'
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ["render_math"]
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Tell Pelican to add 'extra/custom.css' to the output dir
+STATIC_PATHS = ['images', 'extra/CNAME']
+
+# Tell Pelican to change the path to 'static/custom.css' in the output dir
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': '/CNAME'}
+}
+
+# Blogroll
+LINKS = (('Suhas', 'http://suhas.co'),
+         ('Kunal', 'https://github.com/patelkunal'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/coderorg'),
+          ('Github', 'https://github.com/coderearth'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
